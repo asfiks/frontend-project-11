35427ecbc237d59c9006181788a60810182cdf1a
asfiks <asfiks88@gmail.com>
2023-05-11 20:40:07 +0300

    fix first review

--- src/application.js
+++ src/application.js
@@ -20,87 +20,6 @@ const makeProxyLink = (url) => {
   return proxy;
 };
 
-const creatLiForPost = () => {
-  const elementLi = document.createElement('li');
-  elementLi.classList.add('list-group-item', 'd-flex', 'justify-content-between', 'align-items-start', 'border-0', 'border-end-0');
-  return elementLi;
-};
-
-const createElementA = (post) => {
-  const elementA = document.createElement('a');
-  elementA.href = post.link;
-  elementA.textContent = post.title;
-  if (post.status === 'noShowed') {
-    elementA.classList.add('fw-bold');
-  } else {
-    elementA.classList.add('fw-normal');
-  }
-  elementA.setAttribute('data-id', post.id);
-  elementA.setAttribute('target', '_blank');
-  elementA.setAttribute('rel', 'noopener noreferrer');
-  return elementA;
-};
-
-const createButton = (post) => {
-  const button = document.createElement('button');
-  button.type = 'button';
-  button.classList.add('btn', 'btn-outline-primary', 'btn-sm');
-  button.textContent = i18next.t('button');
-  button.setAttribute('data-id', post.id);
-  button.setAttribute('data-bs-toggle', 'modal');
-  button.setAttribute('data-bs-target', '#modal');
-  return button;
-};
-
-const createPost = (post) => {
-  const liElement = creatLiForPost();
-  liElement.append(createElementA(post));
-  liElement.append(createButton(post));
-  return liElement;
-};
-const createNameLists = (text) => {
-  const container = document.createElement('div');
-  container.classList.add('card', 'border-0');
-  const containerForText = document.createElement('div');
-  containerForText.classList.add('card-body');
-  const elementForText = document.createElement('h2');
-  elementForText.classList.add('card-title', 'h4');
-  elementForText.textContent = text;
-  containerForText.append(elementForText);
-  container.append(containerForText);
-  return container;
-};
-const createListForContent = () => {
-  const elementUl = document.createElement('ul');
-  elementUl.classList.add('list-group', 'border-0', 'rounded-0');
-  return elementUl;
-};
-
-const creatFeeds = (feed) => {
-  const li = document.createElement('li');
-  li.classList.add('list-group-item', 'border-0', 'border-end-0');
-  const h3 = document.createElement('h3');
-  h3.classList.add('h6', 'm-0');
-  h3.innerText = feed.title;
-  const p = document.createElement('p');
-  p.classList.add('m-0', 'small', 'text-black-50');
-  p.innerText = feed.description;
-  li.append(h3);
-  li.append(p);
-  return li;
-};
-
-const getTextDanger = (elementFeedback, elementInput, text) => {
-  try {
-    elementFeedback.textContent = text;
-    elementFeedback.classList.remove('text-success');
-    elementFeedback.classList.add('text-danger');
-    elementInput.classList.add('is-invalid');
-  } catch (e) {
-    throw new Error(e);
-  }
-};
-
 const getDataFromURL = (url, state) => {
   const proxyUrl = makeProxyLink(url);
   return axios.get(proxyUrl)
@@ -168,123 +87,6 @@ export const getDataAfterParsing = (state) => {
   return null;
 };
 
-export const renderForFeedback = (state) => {
-  const sectionForm = document.querySelector('.bg-dark');
-  const elementInput = document.querySelector('#url-input');
-  const elementFeedback = sectionForm.querySelector('.feedback');
-  switch (state.validUrl) {
-    case 'errorNetwork':
-      getTextDanger(elementFeedback, elementInput, i18next.t('errorNetwork'));
-      state.validUrl = '';
-      break;
-    case 'noRSS':
-      getTextDanger(elementFeedback, elementInput, i18next.t('noRSS'));
-      state.validUrl = '';
-      break;
-    case 'thereIsRssInState':
-      getTextDanger(elementFeedback, elementInput, i18next.t('thereIsRss'));
-      state.validUrl = '';
-      break;
-    case 'noValid':
-      getTextDanger(elementFeedback, elementInput, i18next.t('noValid'));
-      state.validUrl = '';
-      break;
-    case 'hasRSS':
-      state.stateApp = 'processing';
-      state.validUrl = '';
-      break;
-    case 'rssIsLoad':
-      elementFeedback.textContent = i18next.t('okRSS');
-      elementFeedback.classList.remove('text-danger');
-      elementFeedback.classList.add('text-success');
-      elementInput.value = '';
-      elementInput.focus();
-      state.validUrl = '';
-      break;
-    default:
-      break;
-  }
-};
-
-const renderModal = (state, allButtonView, modalTitle, modalBodyWithText, linkInModal) => {
-  allButtonView.forEach((button) => {
-    button.addEventListener('click', (event) => {
-      const elementWithEvent = event.target.parentNode;
-      const openElementLink = elementWithEvent.querySelector('a');
-      openElementLink.classList.replace('fw-bold', 'fw-normal');
-      const link = openElementLink.getAttribute('href');
-      state.openedLinks.push(link);
-      const [dataForModal] = (state.posts).filter((post) => post.link === link);
-      modalTitle.textContent = dataForModal.title;
-      modalBodyWithText.textContent = dataForModal.description;
-      linkInModal.setAttribute('href', link);
-    });
-  });
-};
-
-const renderFeed = (containerWithListInFeeds, feed) => {
-  containerWithListInFeeds.prepend(creatFeeds(feed));
-};
-
-const renderPosts = (containerWithListInPosts, posts) => {
-  posts.forEach((post) => {
-    containerWithListInPosts.prepend(createPost(post));
-  });
-};
-
-const listenerLinks = (state, allElementsLiInPosts) => {
-  allElementsLiInPosts.forEach((element) => {
-    element.addEventListener('click', (event) => {
-      element.classList.replace('fw-bold', 'fw-normal');
-      const link = (event.target).getAttribute('href');
-      state.openedLinks.push(link);
-    });
-  });
-};
-
-export const render = (state) => {
-  const containerPosts = document.querySelector('.posts');
-  const containerFeeds = document.querySelector('.feeds');
-  const containerModal = document.querySelector('.modal');
-  const modalTitle = containerModal.querySelector('.modal-title');
-  const modalBodyWithText = containerModal.querySelector('.modal-body');
-  const linkInModal = containerModal.querySelector('a');
-  if ((state.urls).length === 1 && state.stateApp === 'processing') {
-    containerPosts.append(createNameLists(i18next.t('posts')));
-    containerFeeds.append(createNameLists(i18next.t('feeds')));
-  }
-  if (state.stateApp === 'processing') {
-    const conteinerWithFeeds = containerFeeds.querySelector('.card');
-    conteinerWithFeeds.append(createListForContent());
-    const containerWithPosts = containerPosts.querySelector('.card');
-    containerWithPosts.append(createListForContent());
-    const feed = state.feeds[0];
-    const { posts } = state;
-    const containerWithListInFeeds = containerFeeds.querySelector('ul');
-    renderFeed(containerWithListInFeeds, feed);
-    const containerWithListInPosts = containerPosts.querySelector('ul');
-    renderPosts(containerWithListInPosts, posts);
-    state.validUrl = 'rssIsLoad';
-    const allButtonView = containerWithListInPosts.querySelectorAll('button');
-    renderModal(state, allButtonView, modalTitle, modalBodyWithText, linkInModal);
-    renderForFeedback(state);
-    state.stateApp = 'processed';
-    return;
-  }
-  if (state.stateApp === 'processed') {
-    const containerWithListInPosts = containerPosts.querySelector('ul');
-    const { posts } = state;
-    while (containerWithListInPosts.firstChild) {
-      containerWithListInPosts.removeChild(containerWithListInPosts.firstChild);
-    }
-    renderPosts(containerWithListInPosts, posts);
-    const allElementsLiInPosts = containerWithListInPosts.querySelectorAll('a');
-    listenerLinks(state, allElementsLiInPosts);
-    const allButtonView = containerWithListInPosts.querySelectorAll('button');
-    renderModal(state, allButtonView, modalTitle, modalBodyWithText, linkInModal);
-  }
-};
-
 export default () => {
   const state = {
     stateApp: 'filling',
@@ -312,7 +114,9 @@ export default () => {
           break;
         case 'hasRSS':
           watchedState.currentUrl = url;
-          watchedState.validUrl = 'hasRSS';
+          state.stateApp = 'processing';
+          state.validUrl = '';
+          getDataAfterParsing(watchedState);
           break;
         case 'noRSS':
           watchedState.validUrl = 'noRSS';
@@ -327,6 +131,7 @@ export default () => {
   });
   const updateData = function updateDataFunction() {
     if (watchedState.stateApp === 'processed') {
+      console.log('update');
       getDataAfterParsing(watchedState);
     }
     setTimeout(updateData, 5000);
--- src/view.js
+++ src/view.js
@@ -1,15 +1,206 @@
 import onChange from 'on-change';
-import { renderForFeedback, render, getDataAfterParsing } from './application.js';
-/* import render from './application.js';
-import getDataAfterParsing from './application.js' */
+import i18next from 'i18next';
+
+const getTextDanger = (elementFeedback, elementInput, text) => {
+  try {
+    elementFeedback.textContent = text;
+    elementFeedback.classList.remove('text-success');
+    elementFeedback.classList.add('text-danger');
+    elementInput.classList.add('is-invalid');
+  } catch (e) {
+    throw new Error(e);
+  }
+};
+
+const renderForFeedback = (state) => {
+  const sectionForm = document.querySelector('.bg-dark');
+  const elementInput = document.querySelector('#url-input');
+  const elementFeedback = sectionForm.querySelector('.feedback');
+  switch (state.validUrl) {
+    case 'errorNetwork':
+      getTextDanger(elementFeedback, elementInput, i18next.t('errorNetwork'));
+      state.validUrl = '';
+      break;
+    case 'noRSS':
+      getTextDanger(elementFeedback, elementInput, i18next.t('noRSS'));
+      state.validUrl = '';
+      break;
+    case 'thereIsRssInState':
+      getTextDanger(elementFeedback, elementInput, i18next.t('thereIsRss'));
+      state.validUrl = '';
+      break;
+    case 'noValid':
+      getTextDanger(elementFeedback, elementInput, i18next.t('noValid'));
+      state.validUrl = '';
+      break;
+    case 'rssIsLoad':
+      elementFeedback.textContent = i18next.t('okRSS');
+      elementFeedback.classList.remove('text-danger');
+      elementFeedback.classList.add('text-success');
+      elementInput.value = '';
+      elementInput.focus();
+      state.validUrl = '';
+      break;
+    default:
+      break;
+  }
+};
+
+const creatFeeds = (feed) => {
+  const li = document.createElement('li');
+  li.classList.add('list-group-item', 'border-0', 'border-end-0');
+  const h3 = document.createElement('h3');
+  h3.classList.add('h6', 'm-0');
+  h3.innerText = feed.title;
+  const p = document.createElement('p');
+  p.classList.add('m-0', 'small', 'text-black-50');
+  p.innerText = feed.description;
+  li.append(h3);
+  li.append(p);
+  return li;
+};
+
+const creatLiForPost = () => {
+  const elementLi = document.createElement('li');
+  elementLi.classList.add('list-group-item', 'd-flex', 'justify-content-between', 'align-items-start', 'border-0', 'border-end-0');
+  return elementLi;
+};
+
+const createElementA = (post) => {
+  const elementA = document.createElement('a');
+  elementA.href = post.link;
+  elementA.textContent = post.title;
+  if (post.status === 'noShowed') {
+    elementA.classList.add('fw-bold');
+  } else {
+    elementA.classList.add('fw-normal');
+  }
+  elementA.setAttribute('data-id', post.id);
+  elementA.setAttribute('target', '_blank');
+  elementA.setAttribute('rel', 'noopener noreferrer');
+  return elementA;
+};
+
+const createButton = (post) => {
+  const button = document.createElement('button');
+  button.type = 'button';
+  button.classList.add('btn', 'btn-outline-primary', 'btn-sm');
+  button.textContent = i18next.t('button');
+  button.setAttribute('data-id', post.id);
+  button.setAttribute('data-bs-toggle', 'modal');
+  button.setAttribute('data-bs-target', '#modal');
+  return button;
+};
+
+const createPost = (post) => {
+  const liElement = creatLiForPost();
+  liElement.append(createElementA(post));
+  liElement.append(createButton(post));
+  return liElement;
+};
+
+const createNameLists = (text) => {
+  const container = document.createElement('div');
+  container.classList.add('card', 'border-0');
+  const containerForText = document.createElement('div');
+  containerForText.classList.add('card-body');
+  const elementForText = document.createElement('h2');
+  elementForText.classList.add('card-title', 'h4');
+  elementForText.textContent = text;
+  containerForText.append(elementForText);
+  container.append(containerForText);
+  return container;
+};
+
+const createListForContent = () => {
+  const elementUl = document.createElement('ul');
+  elementUl.classList.add('list-group', 'border-0', 'rounded-0');
+  return elementUl;
+};
+
+const renderFeed = (containerWithListInFeeds, feed) => {
+  containerWithListInFeeds.prepend(creatFeeds(feed));
+};
+
+const renderPosts = (containerWithListInPosts, posts) => {
+  posts.forEach((post) => {
+    containerWithListInPosts.prepend(createPost(post));
+  });
+};
+
+const listenerLinks = (state, allElementsLiInPosts) => {
+  allElementsLiInPosts.forEach((element) => {
+    element.addEventListener('click', (event) => {
+      element.classList.replace('fw-bold', 'fw-normal');
+      const link = (event.target).getAttribute('href');
+      state.openedLinks.push(link);
+    });
+  });
+};
+
+const renderModal = (state, allButtonView, modalTitle, modalBodyWithText, linkInModal) => {
+  allButtonView.forEach((button) => {
+    button.addEventListener('click', (event) => {
+      const elementWithEvent = event.target.parentNode;
+      const openElementLink = elementWithEvent.querySelector('a');
+      openElementLink.classList.replace('fw-bold', 'fw-normal');
+      const link = openElementLink.getAttribute('href');
+      state.openedLinks.push(link);
+      const [dataForModal] = (state.posts).filter((post) => post.link === link);
+      modalTitle.textContent = dataForModal.title;
+      modalBodyWithText.textContent = dataForModal.description;
+      linkInModal.setAttribute('href', link);
+    });
+  });
+};
+
+const render = (state) => {
+  const containerPosts = document.querySelector('.posts');
+  const containerFeeds = document.querySelector('.feeds');
+  const containerModal = document.querySelector('.modal');
+  const modalTitle = containerModal.querySelector('.modal-title');
+  const modalBodyWithText = containerModal.querySelector('.modal-body');
+  const linkInModal = containerModal.querySelector('a');
+  if ((state.urls).length === 1 && state.stateApp === 'processing') {
+    containerPosts.append(createNameLists(i18next.t('posts')));
+    containerFeeds.append(createNameLists(i18next.t('feeds')));
+  }
+  if (state.stateApp === 'processing') {
+    const conteinerWithFeeds = containerFeeds.querySelector('.card');
+    conteinerWithFeeds.append(createListForContent());
+    const containerWithPosts = containerPosts.querySelector('.card');
+    containerWithPosts.append(createListForContent());
+    const feed = state.feeds[0];
+    const { posts } = state;
+    const containerWithListInFeeds = containerFeeds.querySelector('ul');
+    renderFeed(containerWithListInFeeds, feed);
+    const containerWithListInPosts = containerPosts.querySelector('ul');
+    renderPosts(containerWithListInPosts, posts);
+    state.validUrl = 'rssIsLoad';
+    const allButtonView = containerWithListInPosts.querySelectorAll('button');
+    renderModal(state, allButtonView, modalTitle, modalBodyWithText, linkInModal);
+    renderForFeedback(state);
+    state.stateApp = 'processed';
+    return;
+  }
+  if (state.stateApp === 'processed') {
+    const containerWithListInPosts = containerPosts.querySelector('ul');
+    const { posts } = state;
+    while (containerWithListInPosts.firstChild) {
+      containerWithListInPosts.removeChild(containerWithListInPosts.firstChild);
+    }
+    renderPosts(containerWithListInPosts, posts);
+    const allElementsLiInPosts = containerWithListInPosts.querySelectorAll('a');
+    listenerLinks(state, allElementsLiInPosts);
+    const allButtonView = containerWithListInPosts.querySelectorAll('button');
+    renderModal(state, allButtonView, modalTitle, modalBodyWithText, linkInModal);
+  }
+};
 
 export default (state) => {
   const watchedState = onChange(state, (path, value) => {
     if (path === 'validUrl' && value !== '') {
       renderForFeedback(watchedState);
-    }
-    if (path === 'stateApp' && value === 'processing') {
-      getDataAfterParsing(watchedState);
     }
     if (path === 'posts' && value.length !== 0) {
       render(watchedState);
